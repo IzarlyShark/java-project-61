5322f4c5572bf5cbdcd8ef12b851fbb39218a21c
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2024-05-03 15:59:13 +0300

    error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,15 @@
 plugins {
-    id("java")
+    application
+//    id("java")
 }
-
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
 }
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -17,3 +18,5 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+
